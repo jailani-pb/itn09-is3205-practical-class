bf2e88decc3dc8dda67233dd4222443b96c64b38
Jailani Abdul Rahman <jailani.har@googlemail.com>
2019-08-14 11:11:17 +0800

    Rectangle class

--- week06/slot01/abstraction/Rectangle.java
+++ week06/slot01/abstraction/Rectangle.java
@@ -1,5 +1,46 @@
 package week06.slot01.abstraction;
 
-public class Rectangle {
+public class Rectangle extends Shape2D {
+
+	private double length;
+	private double height;
+	
+	public Rectangle(String colour, double length, double height) {
+		super(colour);
+		setLength(length);
+		setHeight(height);
+	}
+	
+	public double getArea() {
+		return length * height;
+	}
+	
+	public double getPerimeter() {
+		return (2 * length) + (2 * height);
+	}
+	
+	public double getLength() {
+		return length;
+	}
+	
+	public double getHeight() {
+		return height;
+	}
+	
+	public void setLength(double length) {
+		this.length = length;
+		if(this.length < 0) {
+			this.length = 0;
+		}
+	}
+	
+	public void setHeight(double height) {
+		this.height = height;
+		if(this.height < 0) {
+			this.height = 0;
+		}
+	}
+
+
 
 }
